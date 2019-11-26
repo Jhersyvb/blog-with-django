a37d447ccd23607df459e07c3e2f8ca7083bb42a
Jhersy <jhersyvb@gmail.com>
2019-11-26 15:39:29 -0500

    Postgres DB and new requirements

--- blog/settings.py
+++ blog/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoblog_db',
+        'USER': 'djangoblog_admin',
+        'PASSWORD': 'blog@django',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
